4de2548a90cf2bddfe3cda1af6ec82e76e359a29
toastmuseum <Deolijus@gmail.com>
2021-08-04 22:57:22 -0400

    add a command for creating new channels

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord import utils
 
 
 
@@ -38,29 +39,46 @@ async def on_message(message):
      # do something
 
     await client.process_commands(message)
-        
+
 
 ## Bot commands
 
 @client.command()
 async def bot_command(ctx):
     print(f'{client.user} is performing the !bot_command action')
-    
+
     # do something
-    
+
     await ctx.send(f'!bot_command completed')
 
-@client.command()
-async def clear(ctx, amount=15):
-    channel = ctx.message.channel
-    messages = []
-    async for message in channel.history(limit=amount + 1):
-              messages.append(message)
-    
-    print(f'{client.user} has performed !clear action')
-    await channel.delete_messages(messages)
-    await ctx.send(f'{amount} messages have been purged by {ctx.message.author.mention}')
 
+@client.command()
+async def make_channel(ctx, channel_name=None, category_name=None):
+    # !make_channel
+    if not channel_name:
+        await ctx.send(f'No channel name provided.')
+        return
+
+    channel_name = channel_name.replace(' ','-')
+    guild = ctx.guild
+    if category_name:
+        existing_category = utils.get(guild.categories, name = category_name)
+        # Create category if none exists
+        if not existing_category:
+            await guild.create_category(category_name)
+            category_name = utils.get(guild.categories, name = category_name)
+        else:
+            category_name = existing_category
+    else:
+        # Otherwise add channel to the current category
+        category_name = ctx.message.channel.category
+
+    channel = utils.get(category_name.channels, name = channel_name)
+    if channel:
+        await ctx.send(f'Channel <{channel_name}> already exists')
+    else:
+        await guild.create_text_channel(channel_name, category=category_name)
+        await ctx.send(f'Channel <{channel_name}> created!')
 
 
 
