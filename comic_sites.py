fc0cf1e8fd09e9c31b3eebe7aa234bc6370c5064
justin DeOliveira <deoli.justin@gmail.com>
2021-08-06 01:12:41 -0400

    include classes definitions for each website

---
+++ comic_sites.py
@@ -0,0 +1,40 @@
+class ComicSite:
+    def __init__(self):
+        self.name = 'manhuaplus'
+
+    def print_name():
+        return self.name
+    
+
+
+
+class Manhuaplus(ComicSite):
+    def __init__(self):
+        self.name = 'Manhuaplus'
+
+    def get_last_chapter(self):
+        print(f'Retrieving last chapter for {self.name}')
+
+
+class Manhwatop(ComicSite):
+    def __init__(self):
+        self.name = 'Manhwatop'
+
+    def get_last_chapter(self):
+        print(f'Retrieving last chapter for {self.name}')
+
+
+class Flamescans(ComicSite):
+    def __init__(self):
+        self.name = 'Flamescans'
+
+    def get_last_chapter(self):
+        print(f'Retrieving last chapter for {self.name}')
+
+
+class Soloxleveling(ComicSite):
+    def __init__(self):
+        self.name = 'Soloxleveling'
+                
+    def get_last_chapter(self):
+        print(f'Retrieving last chapter for {self.name}')
